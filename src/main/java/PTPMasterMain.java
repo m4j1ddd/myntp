c1d24620bed3ddd2c7cde3a4b538abb2f991044c
majidace <majidabdollahi@ut.ac.ir>
2018-08-05 05:03:30 -0700

    Changing in PTP!

--- src/main/java/PTPMasterMain.java
+++ src/main/java/PTPMasterMain.java
@@ -1,5 +1,30 @@
-public class PTPMasterMain {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.*;
 
+public class PTPMasterMain {
+    public static void main(String[] args) throws IOException {
+        if(args.length >= 2) {
+            String ip = args[0];
+            int port = Integer.valueOf(args[1]);
+            Socket masterSocket = new Socket(ip, port);
+            DataOutputStream outToSlave = new DataOutputStream(masterSocket.getOutputStream());
+            BufferedReader inFromSlave = new BufferedReader(new InputStreamReader(masterSocket.getInputStream()));
+            String slaveSentence;
+
+            Date t1_date = new Date();
+            System.out.println("T1 established!");
+            outToSlave.writeBytes("Sync" + '\n');
+            outToSlave.writeBytes("Follow_Up" + '\n');
+            slaveSentence = inFromSlave.readLine();
+            Date t4_date = new Date();
+            if(slaveSentence.equals("Delay_Req")) {
+                System.out.println("T4 established!");
+                outToSlave.writeBytes("Delay_Resp" + '\n');
+            }
+        }
     }
 }
--- src/main/java/PTPSlaveMain.java
+++ src/main/java/PTPSlaveMain.java
@@ -1,14 +1,37 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.*;
 
 public class PTPSlaveMain {
     public static void main(String[] args) throws IOException {
         if(args.length >= 1) {
             int port = Integer.valueOf(args[0]);
             ServerSocket serverSocket = new ServerSocket(port);
-            Socket socket = serverSocket.accept();
+            Socket slaveSocket = serverSocket.accept();
+            DataOutputStream outToMaster = new DataOutputStream(slaveSocket.getOutputStream());
+            BufferedReader inFromMaster = new BufferedReader(new InputStreamReader(slaveSocket.getInputStream()));
+            String masterSentence;
 
+            masterSentence = inFromMaster.readLine();
+            Date t2_date = new Date();
+            if(masterSentence.equals("Sync")) {
+                System.out.println("T2 established");
+                masterSentence = inFromMaster.readLine();
+                if(masterSentence.equals("Follow_Up")) {
+                    Date t3_date = new Date();
+                    System.out.println("T3 established!");
+                    outToMaster.writeBytes("Delay_Req" + '\n');
+
+                    masterSentence = inFromMaster.readLine();
+                    if(masterSentence.equals("Delay_Resp")) {
+
+                    }
+                }
+            }
         }
     }
 }
