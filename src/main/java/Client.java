305b8617b6ec90e89baf641bc100ba700013dd05
majidace <majidabdollahi@ut.ac.ir>
2018-08-19 02:37:46 -0700

    Client Initial!

---
+++ src/main/java/Client.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Client extends Thread {
+    ServerSocket serverSocket;
+
+    public Client(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void run() {
+        try {
+            while(true) {
+                Socket connectionSocket = serverSocket.accept();
+                BufferedReader inFromOther = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                String msg = inFromOther.readLine();
+                System.out.println("Message received: " + msg);
+                connectionSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String ip, int port, String msg) throws IOException {
+        Socket socket = new Socket(ip, port);
+        DataOutputStream outToOther = new DataOutputStream(socket.getOutputStream());
+        outToOther.writeBytes(msg + '\n');
+        socket.close();
+    }
+}
